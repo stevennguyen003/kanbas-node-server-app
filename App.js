325abdabfef34cf2c914c66c1e41ba404717dbe6
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-04-16 16:00:33 -0400

    fix

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import session from "express-session";
 import express from "express";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
+import Lab5  from "./Lab5.js";
+import Hello from "./Hello.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
@@ -28,5 +32,9 @@ if (process.env.NODE_ENV !== "development") {
 }
 app.use(session(sessionOptions));
 app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Hello(app);
+Lab5(app);
 UserRoutes(app);
 app.listen(4000);
