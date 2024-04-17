124458420b0d2344f789e2d008a3e7ab2db87ba1
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-04-16 23:07:16 -0400

    test

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import session from "express-session";
 import express from "express";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
-import Lab5  from "./Lab5.js";
+import Lab5 from "./Lab5.js";
 import Hello from "./Hello.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
@@ -11,12 +11,10 @@ import cors from "cors";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(
-    cors({
-        origin: ["http://localhost:3000", process.env.FRONTEND_URL],
-        credentials: true,
-    })
-);
+app.use(cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+}));
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
